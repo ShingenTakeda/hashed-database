cd1b58805518f48b00cfbea52d57574d5b141775
rafael.depontes <rafael.depontes@dataprev.gov.br>
2025-02-24 22:00:21 -0300

    ajuste rapido, pq esqueci de testar se estava funcionando com Mock

--- front/script.js
+++ front/script.js
@@ -7,17 +7,20 @@ function showTab(tabId) {
 
 async function loadData() {
   //   const id = document.getElementById("select-id").value;
-  //   const response = await fetch(`https://api.example.com/data/${id}`, {
+  //   const response = await fetch(`https://api.mitsu/hashedDatabase/select/${id}`, {
   //     method: "GET",
   //   });
   //   const data = await response.json();
   //   document.getElementById("result-list").innerHTML = `<li>${data.name}</li>`;
+
+  //mockado pra teste pq tamo sem URL
+  document.getElementById("result-list").innerHTML = `<li>a</li>`;
 }
 
 async function insertData() {
   //   const id = document.getElementById("insert-id").value;
   //   const name = document.getElementById("insert-name").value;
-  //   await fetch("https://api.example.com/data", {
+  //   await fetch("https://api.mitsu/hashedDatabase", {
   //     method: "POST",
   //     headers: { "Content-Type": "application/json" },
   //     body: JSON.stringify({ id, name }),
@@ -28,7 +31,7 @@ async function insertData() {
 async function updateData() {
   //   const id = document.getElementById("update-id").value;
   //   const name = document.getElementById("update-name").value;
-  //   await fetch(`https://api.example.com/data/${id}`, {
+  //   await fetch(`https://api.mitsu/hashedDatabase/update/${id}`, {
   //     method: "PUT",
   //     headers: { "Content-Type": "application/json" },
   //     body: JSON.stringify({ name }),
@@ -38,7 +41,7 @@ async function updateData() {
 
 async function deleteData() {
   //   const id = document.getElementById("delete-id").value;
-  //   await fetch(`https://api.example.com/data/${id}`, {
+  //   await fetch(`https://api.mitsu/hashedDatabase/delete/${id}`, {
   //     method: "DELETE",
   //   });
   //   alert("Deletado com sucesso!");
